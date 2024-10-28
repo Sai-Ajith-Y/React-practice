2a5b572804ec4ed9ea084bf12ff42d979a2b340b
Ajith Yeluri <yvrsajith@outlook.com>
2024-10-28 18:33:09 -0500

    Added 3rd Project

---
+++ React_Project_3/app.js
@@ -0,0 +1,31 @@
+/* <div id="parent">
+          <div id="child">
+               <h1> i am a h1 tag</h1>
+               <h2> i am a h2 tag</h2>
+          </div>
+          <div id="child2">
+               <h1> i am a h1 tag</h1>
+               <h2> i am a h2 tag</h2>
+          </div>
+   </div>
+   we need to use array for the parent object to pass
+    multiple div childs
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div",{id: "parent"},[
+     React.createElement("div",{id: "child"},[
+          React.createElement("h1",{},"I,m a h tag"),
+          React.createElement("h1",{},"I,m a h2 tag")
+     ]),
+     React.createElement("div",{id: "child"},[
+          React.createElement("h1",{},"I,m a h1 tag"),
+          React.createElement("h1",{},"I,m a h2 tag")
+     ])
+]
+     
+)     
+        
+console.log(parent); // object       
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
